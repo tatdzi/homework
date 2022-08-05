549e090fceeb675ea71719a4f5d2bfcdd43a1ac9
Dzianis Tatur <tatur95@gmail.com>
2022-08-05 15:36:24 +0300

    Exercises before 4.3

--- src/home_work_1/AverageNumberSearch.java
+++ src/home_work_1/AverageNumberSearch.java
@@ -1,2 +1,26 @@
-package home_work_1;public class AverageNumberSearch {
+package home_work_1;
+
+public class AverageNumberSearch {
+    public static void main(String[] args){
+        int a = 30;
+        int b = 20;
+        int c = 40;
+
+        if (a > b) {
+            if (a < c) {
+                System.out.println("Среднее число а = " + a);
+            } else if (c > b) {
+                System.out.println("Среднее число c = " + c);
+            } else {
+                System.out.println("Среднее число b = " + b);
+            }
+        }else if (b < c){
+            System.out.println("Среднее число b = " + b);
+        }else if (a < c){
+            System.out.println("Среднее число c = " + c);
+        }else {
+            System.out.println("Среднее число a = " + a);
+        }
+
+    }
 }
