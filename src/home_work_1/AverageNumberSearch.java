411551a0aa110640a5615806a023cf88d8500a6c
Dzianis Tatur <tatur95@gmail.com>
2022-08-10 10:21:46 +0300

    improve AverageNumberSearch class

--- src/home_work_1/AverageNumberSearch.java
+++ src/home_work_1/AverageNumberSearch.java
@@ -5,26 +5,59 @@ import java.util.Scanner;
 public class AverageNumberSearch {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите 3 числа через enter!");
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
+        System.out.println("Введите 3 разных числа через enter!");
+        int a = enterAndChackInt(in);
+        int b = enterAndChackInt(in);
+        int c = enterAndChackInt(in);
+        while (!(a != b && b != c && a != c)){
+            System.out.println("Введненные числа не удовлетворяют" +
+                    " требовония!\nПопробуйте еще раз!");
+            a = enterAndChackInt(in);
+            b = enterAndChackInt(in);
+            c = enterAndChackInt(in);
+        }
+        int result =averageNumber(a,b,c);
+        System.out.println("Среднее из трех чисел: "+result);
+    }
 
+    /**
+     * Метод enterAndChackInt проверяет можно ли интерпретировать введенное значение как int.
+     * и присваивает его.
+     * @param input Scanner
+     * @return переменная int
+     */
+    public static int enterAndChackInt(Scanner input) {
+        while (!input.hasNextInt()) {
+            System.out.println("Это очень большое число или не являетя им\n" +
+                    "Попробуйте еще раз!");
+            input.next();
+        }
+        return input.nextInt();
+    }
+
+    /**
+     * Метод averageNumber определяет какое из чисел среднее из трех РАЗНЫХ числе.
+     * Если 2 и более числа будут одинаковые, метод будет работать некорректно!
+     * @param a Число
+     * @param b Число
+     * @param c Число
+     * @return Среднее из чисел.
+     */
+    public static int averageNumber(int a,int b, int c){
         if (a > b) {
             if (a < c) {
-                System.out.println("Среднее число " + a);
+                return a;
             } else if (c > b) {
-                System.out.println("Среднее число " + c);
+                return c;
             } else {
-                System.out.println("Среднее число " + b);
+                return b;
             }
         }else if (b < c){
-            System.out.println("Среднее число " + b);
+            return b;
         }else if (a < c){
-            System.out.println("Среднее число " + c);
+            return c;
         }else {
-            System.out.println("Среднее число " + a);
+            return a;
         }
-
     }
 }
